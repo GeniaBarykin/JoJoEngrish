ca881da82573d056c8b099e574dfca9a5d0d7295
GeniaBarykin <467232@student.saxion.nl>
2020-04-18 08:37:16 +0200

    Search engine improvements

--- app/src/main/java/com/evgeny/app/jojoengrish/search_engine/SearchEngine.java
+++ app/src/main/java/com/evgeny/app/jojoengrish/search_engine/SearchEngine.java
@@ -7,68 +7,63 @@ import java.util.ArrayList;
 
 public class SearchEngine {
 
-    public static ArrayList<SoundModel> findSoundFiles(String searchBarText){
+    public static ArrayList<SoundModel> findSoundFiles(String searchBarText) {
         ArrayList<SoundModel> sounds = DataManager.request().getSounds();
         ArrayList<SoundModel> sortedSounds = new ArrayList<>();
 
-        searchBarText=searchBarText.toLowerCase();
+        searchBarText = searchBarText.toLowerCase();
         String[] tryTags = searchBarText.split(" ");
-        int[]matches =new int[sounds.size()];
+        int[] matches = new int[sounds.size()];
         int index = 0;
-        int value =0;
+        int value = 0;
+        int maxValue = Integer.MIN_VALUE;
+        int maxIndex = Integer.MIN_VALUE;
         for (SoundModel sound :
                 sounds) {
             for (String tryTag :
                     tryTags) {
-                if(tryTag.equals(sound.getName())){
+                if (sound.getName().contains(tryTag)) {
                     value++;
                 }
                 for (String tag :
                         sound.getTags()) {
-                    if(tryTag.equals(tag)){
+                    if (tag.contains(tryTag)) {
                         value++;
                     }
                 }
             }
-            matches[index]=value;
-            value=0;
+            if (maxValue < value) {
+                maxValue = value;
+                maxIndex = sounds.indexOf(sound);
+            }
+            matches[index] = value;
+            value = 0;
             index++;
         }
-        int []sortedIndexes =sortIndexes(matches,sounds.size());
-        for (int i = 0; i < tryTags.length; i++) {
-            int currentValue = matches[sortedIndexes[i]];
-            if(currentValue!=0) {
-                sortedSounds.add(sounds.get(sortedIndexes[i]));
+        sortedSounds.add(sounds.get(maxIndex));
+        maxValue = Integer.MIN_VALUE;
+        maxIndex = Integer.MIN_VALUE;
+
+        for (int i = 0; i < sounds.size(); i++) {
+            for (SoundModel sound :
+                    sounds) {
+                if (!sortedSounds.contains(sound)) {
+                    int soundValue = matches[sounds.indexOf(sound)];
+                    if (soundValue != 0 && soundValue > maxValue) {
+                        maxValue = soundValue;
+                        maxIndex = sounds.indexOf(sound);
+                    }
+
+                }
             }
+            if (maxValue <= 0) {
+                break;
+            }
+            sortedSounds.add(sounds.get(maxIndex));
+            maxValue = Integer.MIN_VALUE;
+            maxIndex = Integer.MIN_VALUE;
         }
         return sortedSounds;
     }
 
-    private static int[]sortIndexes (int[] matches, int size){
-        int [] sortedIndexes = new int [size];
-        int max = 0;
-        int maxID = 0;
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if(matches[j]>max && !contatins(sortedIndexes,matches[j])){
-                    max=matches[j];
-                    maxID=j;
-                }
-            }
-            sortedIndexes[i]= maxID;
-            maxID=0;
-        }
-        return sortedIndexes;
-
-    }
-
-    private static boolean contatins(int [] array, int value){
-        for (int i = 0; i < array.length; i++) {
-            if(array[i]==value){
-                return true;
-            }
-        }
-        return false;
-
-    }
 }
